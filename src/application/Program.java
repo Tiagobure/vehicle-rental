b8d0626e5cfef8bf9a93ecde9d3528f07515d7da
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-11 13:25:11 -0300

    implementacao sem inteface

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,9 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 
@@ -13,17 +16,37 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"); 
+	DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"); 
 		
 		System.out.println("Enter the rental details");
-		System.out.print("Car model");
+		System.out.print("Car model: ");
 		String carModel = sc.nextLine();
-		System.out.println("withdrawal (dd/MM/yyyy hh:mm)");
+		System.out.print("withdrawal (dd/MM/yyyy hh:mm): ");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.println(" return(dd/MM/yyyy hh:mm)");
+		System.out.print(" return(dd/MM/yyyy hh:mm): ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 		
-		CarRental cr = new CarRental()
+		CarRental cr = new CarRental(start, finish,new Vehicle(carModel));
+		
+System.out.print("enter the hourly price: ");
+double pricePerHour = sc.nextDouble();
+System.out.print("Enter the day price: ");
+double pricePerDay = sc.nextDouble();
+
+RentalService rentalService = new RentalService(pricePerHour, pricePerDay,new BrazilTaxService());
+
+rentalService.processInvoice(cr);
+
+System.out.println("INVOICE");
+System.out.println("Basic payment " + cr.getInvoice().getBasicPayment());
+System.out.println("TAX: " + cr.getInvoice().getTax());
+System.out.println("Total payment: " + cr.getInvoice().getTotalPayment());
+
+
+
+
+	
+		
 	}
 
 }
